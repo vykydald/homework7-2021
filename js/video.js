f117a0a526a951b7273be6c723cc64614c5000c2
vykydald <89817948+vykydald@users.noreply.github.com>
2021-11-17 17:49:09 -0500

    Initial attempt at finishing hw7

---
+++ js/video.js
@@ -0,0 +1,70 @@
+var video = document.querySelector(".video");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = document.querySelector("#slider").value + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log("New speed is", video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log("New speed is", video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Original location", video.currentTime);
+	let newTime = video.currentTime + 15;
+	if (newTime >= video.duration) {
+		video.currentTime = 0;
+		console.log("Going back to beginning");
+	}
+	else {
+		video.currentTime = video.currentTime + 15
+	} 
+	console.log("New location", video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (document.querySelector("#mute").innerHTML == "Mute") {
+		video.volume = 0.0;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else if (document.querySelector("#mute").innerHTML == "Unmute"){
+		video.volume = 1.0;
+		document.querySelector("#mute").innerHTML = "Mute";
+	};
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	let newVolume_decimal = document.querySelector("#slider").value / 100; 
+	let newVolume = newVolume_decimal * 100;
+	video.volume = newVolume_decimal;
+	document.querySelector("#slider").value = newVolume;
+	document.querySelector("#volume").innerHTML = newVolume + "%";
+	console.log(newVolume_decimal);
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.setAttribute("class", "oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.setAttribute("class", "video");
+});
+
+
+
